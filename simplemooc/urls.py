224004a4ed67070f8298659ef21ded80a3dbf2bb
José Anderson <juniordkj90@gmail.com>
2022-03-09 15:11:02 -0300

    Add: Imagem padrão para cursos sem imagem e imagem para cada curso se foram adicionadas na aba admin em ambiente de produção

--- simplemooc/urls.py
+++ simplemooc/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('simplemooc.core.urls')), #Essa linha de comando vai linkar o arquivo de urls da pasta do projeto
     path('courses/', include('simplemooc.courses.urls')),  #para o arquivo urls da pasta core, lá vamos encontrar as demais urls a serem usadas
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
