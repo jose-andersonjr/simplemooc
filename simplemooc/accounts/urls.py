834f91bafe021302b1e6b363aadc480692ef3e85
José Anderson <juniordkj90@gmail.com>
2022-03-23 23:10:28 -0300

    Adicionadas funções de cadastrar usuário, login e logout

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,9 +1,11 @@
+from django.shortcuts import redirect
 from django.urls import include, path
 from simplemooc.accounts import views
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('entrar', LoginView.as_view(), {'template_name':'registration/login.html'}, name='login'),
+    path('entrar', LoginView.as_view(next_page='home'), name='login'),
     path('cadastre-se', views.register, name="register"),
+    path('logout', LogoutView.as_view(next_page='home'), name="logout"),
 
 ]
